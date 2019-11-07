25de0e0510a1e126a83e3cb7d5f983a26d5bd750
sv.junic <j-fujimura@imj.co.jp>
2019-11-07 21:23:40 +0900

    マルチプロセス化

--- getCapture.js
+++ getCapture.js
@@ -1,79 +1,115 @@
 const puppeteer = require('puppeteer');
 
-async function scrollToBottom(page, viewportHeight) {
-  const getScrollHeight = () => {
-    return Promise.resolve(document.documentElement.scrollHeight);
-  };
+process.on('exit', function() {
+  console.log('child exit');
+});
 
-  let scrollHeight = await page.evaluate(getScrollHeight);
-  let currentPosition = 0;
-  let scrollNumber = 0;
+//process.on('message', async function({ url, output }, processExit = false) {
+process.on('message', async function(data) {
+  const threadNumber = data.threadNumber;
+  if (data.query === undefined) {
+    process.send({
+      message: 'exit',
+      thread: threadNumber
+    });
+    process.exit();
+    return;
+  }
 
-  while (currentPosition < scrollHeight) {
-    scrollNumber += 1;
-    const nextPosition = scrollNumber * viewportHeight;
-    await page.evaluate(function(scrollTo) {
-      return Promise.resolve(window.scrollTo(0, scrollTo));
-    }, nextPosition);
+  const { url, output } = data.query;
+  const processExit = data.processExit;
+
+  async function scrollToBottom(page, viewportHeight) {
+    const getScrollHeight = () => {
+      return Promise.resolve(document.documentElement.scrollHeight);
+    };
+
+    let scrollHeight = await page.evaluate(getScrollHeight);
+    let currentPosition = 0;
+    let scrollNumber = 0;
+
+    while (currentPosition < scrollHeight) {
+      scrollNumber += 1;
+      const nextPosition = scrollNumber * viewportHeight;
+      await page.evaluate(function(scrollTo) {
+        return Promise.resolve(window.scrollTo(0, scrollTo));
+      }, nextPosition);
+
+      await page
+        .waitForNavigation({
+          waitUntil: 'networkidle2',
+          timeout: 50
+        })
+        .catch(e => {
+          //console.log('timeout exceed. proceed to next operation')
+        });
+
+      currentPosition = nextPosition;
+      //console.log(`scrollNumber: ${scrollNumber}`);
+      //console.log(`currentPosition: ${currentPosition}`);
+
+      // 2
+      scrollHeight = await page.evaluate(getScrollHeight);
+      //console.log(`ScrollHeight ${scrollHeight}`);
+    }
+  }
+
+  async function getCapture() {
+    const viewportHeight = 1200;
+    const viewportWidth = 1200;
+    const browser = await puppeteer.launch({
+      headless: true
+    });
+
+    const page = await browser.newPage();
+    page.setViewport({
+      width: viewportWidth,
+      height: viewportHeight
+    });
+
+    await page.goto(url);
+
+    // 1
+    await page
+      .waitForNavigation({
+        waitUntil: 'networkidle0'
+        //timeout: 10000
+      })
+      .catch(e => {
+        // console.log('timeout exceed. proceed to next operation'));
+      });
+
+    await scrollToBottom(page, viewportHeight);
 
     await page
       .waitForNavigation({
         waitUntil: 'networkidle2',
-        timeout: 50
+        timeout: 500
       })
-      .catch(e => console.log('timeout exceed. proceed to next operation'));
+      .catch(e => {
+        // console.log('timeout exceed. proceed to next operation');
+      });
 
-    currentPosition = nextPosition;
-    console.log(`scrollNumber: ${scrollNumber}`);
-    console.log(`currentPosition: ${currentPosition}`);
+    await page.screenshot({
+      path: output,
+      fullPage: true
+    });
 
-    // 2
-    scrollHeight = await page.evaluate(getScrollHeight);
-    console.log(`ScrollHeight ${scrollHeight}`);
+    console.log('save screenshot: ' + url);
+    await browser.close();
   }
-}
 
-async function getCapture({ url, output }) {
-  console.log(url, output);
+  console.log('start screenshot: ' + url);
+  console.log(data.query);
 
-  const viewportHeight = 1200;
-  const viewportWidth = 1200;
-  const browser = await puppeteer.launch({
-    headless: true
+  await getCapture();
+
+  process.send({
+    message: 'fix',
+    thread: threadNumber
   });
 
-  const page = await browser.newPage();
-  page.setViewport({
-    width: viewportWidth,
-    height: viewportHeight
-  });
-
-  await page.goto(url);
-
-  // 1
-  await page
-    .waitForNavigation({
-      waitUntil: 'networkidle0'
-      //timeout: 10000
-    })
-    .catch(e => console.log('timeout exceed. proceed to next operation'));
-
-  await scrollToBottom(page, viewportHeight);
-
-  await page
-    .waitForNavigation({
-      waitUntil: 'networkidle2',
-      timeout: 500
-    })
-    .catch(e => console.log('timeout exceed. proceed to next operation'));
-
-  await page.screenshot({
-    path: output,
-    fullPage: true
-  });
-
-  console.log('save screenshot: ' + url);
-  await browser.close();
-}
-
-module.exports = getCapture;
+  if (processExit) {
+    process.exit();
+  }
+});
--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-const puppeteer = require('puppeteer');
-const getCapture = require('./getCapture.js');
-
-const execSync = require('child_process').execSync;
-
 const fs = require('fs');
+const puppeteer = require('puppeteer');
+
+const { fork, execSync } = require('child_process');
+
+//const getCapture = require('./getCapture.js');
 
 const { PATH_LIST, HOSTS } = require('./config.js');
 
 const dirlist = ['./results/production', './results/development', './results/cli', './results/diff'];
-
 for (let i = 0; i < dirlist.length; ++i) {
   let path = dirlist[i];
   if (!fs.existsSync(path)) {
@@ -16,64 +15,75 @@ for (let i = 0; i < dirlist.length; ++i) {
   }
 }
 
-let cliargv = [...process.argv];
-cliargv.shift(); // node
-cliargv.shift(); // runnnig script path
+const SUBROUTINE_SCRIPT_PATH = './getCapture.js';
 
-if (cliargv && cliargv.length > 0) {
-  exec_capturing_cli();
-} else {
-  exec_capturing_reg();
-}
+function createQuery() {
+  const queries = [];
+  for (let i = 0; i < PATH_LIST.length; ++i) {
+    let url = PATH_LIST[i];
 
-async function exec_capturing_cli(url) {
-  for (let i = 0; i < urlLists.length; ++i) {
-    let url = urlLists[i];
-    getCapture({
-      url: url,
-      output: `results/cli/${url
+    queries.push({
+      url: `${HOSTS.production}${url}`,
+      output: `results/production/${url
+        .replace(/:/g, '')
+        .replace(/\/$/g, '_index.html')
+        .replace(/\//g, '_')
+        .replace(/^_/g, '')}.png`
+    });
+
+    queries.push({
+      url: `${HOSTS.development}${url}`,
+      output: `results/development/${url
         .replace(/:/g, '')
         .replace(/\/$/g, '_index.html')
         .replace(/\//g, '_')
         .replace(/^_/g, '')}.png`
     });
   }
+
+  return queries;
 }
-async function exec_capturing_reg() {
-  for (let i = 0; i < PATH_LIST.length; ++i) {
-    let promises = [];
 
-    let url = PATH_LIST[i];
-    console.log(url);
+function getCapture(threadNumber) {
+  const query = queries.length > 0 ? queries.shift() : undefined;
+  const processExit = queries.length < childs.length;
+  childs[threadNumber].send({ query: query, processExit: processExit, threadNumber: threadNumber });
+}
 
-    promises.push(
-      getCapture({
-        url: `${HOSTS.production}${url}`,
-        output: `results/production/${url
-          .replace(/:/g, '')
-          .replace(/\/$/g, '_index.html')
-          .replace(/\//g, '_')
-          .replace(/^_/g, '')}.png`
-      })
-    );
+// 他プロセスの作成
+const CPUs = require('os').cpus().length;
+const childs = [];
+for (let i = 0; i < CPUs; ++i) {
+  let child = fork(SUBROUTINE_SCRIPT_PATH);
+  child.on('message', function(data) {
+    console.log(data);
+    if (queries.length === 0) {
+      console.log('thread exit');
+      exec_reg();
+      return;
+    }
 
-    promises.push(
-      getCapture({
-        url: `${HOSTS.development}${url}`,
-        output: `results/development/${url
-          .replace(/:/g, '')
-          .replace(/\/$/g, '_index.html')
-          .replace(/\//g, '_')
-          .replace(/^_/g, '')}.png`
-      })
-    );
+    if (queries.length > 0) {
+      getCapture(data.threadNumber);
+    }
+  });
+  childs.push(child);
+}
 
-    await Promise.all(promises);
-  }
+// クエリの生成
+const queries = createQuery();
 
+// 実行
+for (let i = 0; i < childs.length; ++i) {
+  console.log('threadNumber', i);
+  getCapture(i);
+}
+
+function exec_reg() {
   try {
     //reg
     execSync('node ./node_modules/reg-cli/dist/cli.js ./results/development/ ./results/production/ ./results/diff/ -R ./results/report.html');
+    process.exit();
   } catch (err) {
     err.stdout;
     err.stderr;
